7d1a31bca04bd74b2dd7280ec065c74b7997ccca
MostafaxSherif <mostafasherif8989@gmail.com>
2025-02-04 06:16:16 +0200

    first commit

---
+++ assets/js/projetos.js
@@ -0,0 +1,168 @@
+function projetos(section) {
+  section.innerHTML = `<header>
+      <h2>Projects</h2>
+    </header>
+    <div class="content">
+      <div class="cards">
+        <div class="card">
+          <h3>Dina Farms Content Marketing Plan</h3>
+          <div class="img-wrapper">
+            <img
+              src="./assets/images/projects/Herd of Cow on Green Grass Field · Free Stock Photo.png"
+            />
+            <div class="buttons">
+              <a href="https://drive.google.com/file/d/14JAlCBN1XuXOtgrKY5F-kMaiC-w8bv5_/view?usp=sharing" target="_blank" title="View Project">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  width="24"
+                  height="24"
+                >
+                  <path fill="none" d="M0 0h24v24H0z" />
+                  <path
+                    d="M12 3c5.392 0 9.878 3.88 10.819 9-.94 5.12-5.427 9-10.819 9-5.392 0-9.878-3.88-10.819-9C2.121 6.88 6.608 3 12 3zm0 16a9.005 9.005 0 0 0 8.777-7 9.005 9.005 0 0 0-17.554 0A9.005 9.005 0 0 0 12 19zm0-2.5a4.5 4.5 0 1 1 0-9 4.5 4.5 0 0 1 0 9zm0-2a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5z"
+                  />
+                </svg>
+              </a>
+            </div>
+          </div>
+          <p>
+            Develop content that resonates with the health-conscious consumer interested in fresh, organic, and sustainable food options.
+          </p>
+        </div>
+        <div class="card">
+          <h3>Kitro's Content Marketing Plan</h3>
+          <div class="img-wrapper">
+            <img
+              src="./assets/images/projects/KITRO’S CONTENT MARKETING PLAN.png"
+            />
+            <div class="buttons">
+            <a href="https://www.instagram.com/kitro.eg?igsh=MWpicW9qZzR3Z3cyZA==" target="_blank" title="Instgram">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  width="24"
+                  height="24"
+                >
+                  <path fill="white"/>
+                  <path
+                    d="M7 2C4.23858 2 2 4.23858 2 7V17C2 19.7614 4.23858 22 7 22H17C19.7614 22 22 19.7614 22 17V7C22 4.23858 19.7614 2 17 2H7ZM16.5 5C16.7761 5 17 5.22386 17 5.5C17 5.77614 16.7761 6 16.5 6C16.2239 6 16 5.77614 16 5.5C16 5.22386 16.2239 5 16.5 5ZM12 7C14.7614 7 17 9.23858 17 12C17 14.7614 14.7614 17 12 17C9.23858 17 7 14.7614 7 12C7 9.23858 9.23858 7 12 7ZM12 9C10.3431 9 9 10.3431 9 12C9 13.6569 10.3431 15 12 15C13.6569 15 15 13.6569 15 12C15 10.3431 13.6569 9 12 9Z"/>
+                  />
+                </svg>
+              </a>
+              <a href="https://drive.google.com/file/d/1sAIfBduhY4eW-jsLaEVQiTs4y2j_k76v/view?usp=sharing">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  width="24"
+                  height="24"
+                >
+                  <path fill="none" d="M0 0h24v24H0z" />
+                  <path
+                    d="M12 3c5.392 0 9.878 3.88 10.819 9-.94 5.12-5.427 9-10.819 9-5.392 0-9.878-3.88-10.819-9C2.121 6.88 6.608 3 12 3zm0 16a9.005 9.005 0 0 0 8.777-7 9.005 9.005 0 0 0-17.554 0A9.005 9.005 0 0 0 12 19zm0-2.5a4.5 4.5 0 1 1 0-9 4.5 4.5 0 0 1 0 9zm0-2a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5z"
+                  />
+                </svg>
+              </a>
+            </div>
+          </div>
+          <p>
+            Create and distribute high-quality content to build awareness of kitro’s products and values.
+          </p>
+        </div>
+        <div class="card">
+          <h3>KATAKITO Content Marketing Plan</h3>
+          <div class="img-wrapper">
+            <img src="./assets/images/projects/KATAKITO.png"/>
+            <div class="buttons">
+              <a href="https://www.instagram.com/katakito.unofficial?igsh=MXcxcXkyazd5ZTU3ZQ==" target="_blank" title="Instgram">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  width="24"
+                  height="24"
+                >
+                  <path fill="white"/>
+                  <path
+                    d="M7 2C4.23858 2 2 4.23858 2 7V17C2 19.7614 4.23858 22 7 22H17C19.7614 22 22 19.7614 22 17V7C22 4.23858 19.7614 2 17 2H7ZM16.5 5C16.7761 5 17 5.22386 17 5.5C17 5.77614 16.7761 6 16.5 6C16.2239 6 16 5.77614 16 5.5C16 5.22386 16.2239 5 16.5 5ZM12 7C14.7614 7 17 9.23858 17 12C17 14.7614 14.7614 17 12 17C9.23858 17 7 14.7614 7 12C7 9.23858 9.23858 7 12 7ZM12 9C10.3431 9 9 10.3431 9 12C9 13.6569 10.3431 15 12 15C13.6569 15 15 13.6569 15 12C15 10.3431 13.6569 9 12 9Z"/>
+                  />
+                </svg>
+              </a>
+              <a href="https://drive.google.com/file/d/1vW2ZGH-kCWgTkaRP1tV6_bfkvzK7m18Q/view?usp=sharing" target="_blank" title="View Project">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  width="24"
+                  height="24"
+                >
+                  <path fill="none" d="M0 0h24v24H0z" />
+                  <path
+                    d="M12 3c5.392 0 9.878 3.88 10.819 9-.94 5.12-5.427 9-10.819 9-5.392 0-9.878-3.88-10.819-9C2.121 6.88 6.608 3 12 3zm0 16a9.005 9.005 0 0 0 8.777-7 9.005 9.005 0 0 0-17.554 0A9.005 9.005 0 0 0 12 19zm0-2.5a4.5 4.5 0 1 1 0-9 4.5 4.5 0 0 1 0 9zm0-2a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5z"
+                  />
+                </svg>
+              </a>
+            </div>
+          </div>
+          <p>Develop and implement content that communicates and remind people of Katakito’s core values, unique selling propositions (USPs), and product offerings.</p>
+        </div>
+        <div class="card">
+          <h3>Smoochy Content Marketing Plan</h3>
+          <div class="img-wrapper">
+            <img src="./assets/images/projects/pr strategies.png" />
+            <div class="buttons">
+            <a href="https://drive.google.com/file/d/1Q3JM-LpK7uHuGoV7Bs3-Pqs-fGfcSRrV/view?usp=sharing" target="_blank" title="View Project">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  width="24"
+                  height="24"
+                >
+                  <path fill="none" d="M0 0h24v24H0z" />
+                  <path
+                    d="M12 3c5.392 0 9.878 3.88 10.819 9-.94 5.12-5.427 9-10.819 9-5.392 0-9.878-3.88-10.819-9C2.121 6.88 6.608 3 12 3zm0 16a9.005 9.005 0 0 0 8.777-7 9.005 9.005 0 0 0-17.554 0A9.005 9.005 0 0 0 12 19zm0-2.5a4.5 4.5 0 1 1 0-9 4.5 4.5 0 0 1 0 9zm0-2a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5z"
+                  />
+                </svg>
+              </a>
+            </div>
+          </div>
+          <p>A based realtime content marketing plan to grab attention  , relate with the realtime to connect more with  the targeted audience and also to drive sales.</p>
+        </div>
+        <div class="card">
+          <h3>Hobbytogether Content Marketing Plan</h3>
+          <div class="img-wrapper">
+            <img src="./assets/images/projects/Hobbytogetherr.png" alt="Imagem do projeto." />
+            <div class="buttons">
+              <a href="https://www.instagram.com/hobbytogether_?igsh=aW0wZTM5MWd1OHpk" target="_blank" title="Instgram">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  width="24"
+                  height="24"
+                >
+                  <path fill="white"/>
+                  <path
+                    d="M7 2C4.23858 2 2 4.23858 2 7V17C2 19.7614 4.23858 22 7 22H17C19.7614 22 22 19.7614 22 17V7C22 4.23858 19.7614 2 17 2H7ZM16.5 5C16.7761 5 17 5.22386 17 5.5C17 5.77614 16.7761 6 16.5 6C16.2239 6 16 5.77614 16 5.5C16 5.22386 16.2239 5 16.5 5ZM12 7C14.7614 7 17 9.23858 17 12C17 14.7614 14.7614 17 12 17C9.23858 17 7 14.7614 7 12C7 9.23858 9.23858 7 12 7ZM12 9C10.3431 9 9 10.3431 9 12C9 13.6569 10.3431 15 12 15C13.6569 15 15 13.6569 15 12C15 10.3431 13.6569 9 12 9Z"/>
+                  />
+                </svg>
+              </a>
+              <a href="https://drive.google.com/file/d/1_zsMGQAYva741b0K_xAVF0lnCXcaS5w2/view?usp=sharing" target="_blank" title="View Project">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  width="24"
+                  height="24"
+                >
+                  <path fill="none" d="M0 0h24v24H0z" />
+                  <path
+                    d="M12 3c5.392 0 9.878 3.88 10.819 9-.94 5.12-5.427 9-10.819 9-5.392 0-9.878-3.88-10.819-9C2.121 6.88 6.608 3 12 3zm0 16a9.005 9.005 0 0 0 8.777-7 9.005 9.005 0 0 0-17.554 0A9.005 9.005 0 0 0 12 19zm0-2.5a4.5 4.5 0 1 1 0-9 4.5 4.5 0 0 1 0 9zm0-2a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5z"
+                  />
+                </svg>
+              </a>
+            </div>
+          </div>
+          <p>A content plan was made to Increase awareness , focus on the unique experience that the user will get to attract the target audience and generate more engagement.</p>
+        </div>
+      </div>
+    </div>`;
+}
+
+export { projetos };
